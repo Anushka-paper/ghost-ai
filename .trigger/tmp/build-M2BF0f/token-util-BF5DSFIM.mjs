d66417ddeb592f2d540e870c27bba6b760ca16d3
Anushka-paper <anushka010804@gmail.com>
2026-06-01 14:42:59 +0530

    Fix: Move prisma migrate out of postinstall to fix Trigger.dev build

--- .trigger/tmp/build-M2BF0f/token-util-BF5DSFIM.mjs
+++
@@ -1,6 +0,0 @@
-import {
-  require_token_util
-} from "./chunk-ZAPSEY2N.mjs";
-import "./chunk-TQ3WNEB5.mjs";
-export default require_token_util();
-//# sourceMappingURL=token-util-BF5DSFIM.mjs.map
